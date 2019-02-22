aa6c6cded2f6b0ac9c6fab6c998d4d3b3a4aa996
AVTahion <avkontakt40@gmail.com>
2019-02-22 23:21:41 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  1)  Дан целочисленный  массив из 20 элементов.Элементы массива  могут принимать  целые значения  от –10 000 до 10 000 включительно.
+ *      Заполнить случайными числами.Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых 
+ *      только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива.Например, для массива 
+ *      из пяти элементов: 6; 2; 9; –3; 6 ответ — 2. 
+*/
+
+namespace Task_1
+{
+    class Program
+    {
+        private static int CheckPairs(int[] array)
+        {
+            int amount = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] % 3 == 0 ^ array[i] % 3 == 0)
+                {
+                    amount++;
+                }
+            }
+
+            return amount;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10000, 10000);
+            }
+            int x = CheckPairs(array);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
